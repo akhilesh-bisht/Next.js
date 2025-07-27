f9caa3b39531c23ab6313af178c0f0c5aa1b7611
Akhilesh Bisht <akhileshbisht2020@gmail.com>
2025-07-27 22:40:44 +0530

    nested layouts

---
+++ routing-demo/src/app/footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function footer() {
+  return (
+    <div style={{background:'red'}}>footer</div>
+  )
+}
+
+export default footer
---
+++ routing-demo/src/app/product/[productId]/layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Layout({children}) {
+  return (
+  <>
+
+  {children}
+  <h2>this is nested layouts</h2>
+  </>
+  )
+}
+
+export default Layout
