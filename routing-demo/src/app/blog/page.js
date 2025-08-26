363e29b1ad805dac907273337bae4c078a8efc08
Akhilesh Bisht <akhileshbisht2020@gmail.com>
2025-08-26 19:39:58 +0530

    loading ui

--- routing-demo/src/app/blog/page.js
+++ routing-demo/src/app/blog/page.js
@@ -1,7 +1,17 @@
 import Link from "next/link"
-function page() {
+ async function page  () {
+  await new Promise((resolve , reject)=>{
+    setTimeout(() => {
+      resolve("intentional delay")
+    }, 2000);
+  })
   return (<>
+  
+
     <div><h1>BLOG PAGE!</h1></div>
+    <p>
+    this is a blog page to see the all blog and redirect the blog details page
+    </p>
     <Link href="/product">product</Link>
   </>
   )
